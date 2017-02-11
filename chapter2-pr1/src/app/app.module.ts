6a2f112d6a98e0565a8023af05e3b3952ef22895
AleksBro <aleksandr.brova@yandex.ru>
2017-02-11 15:41:11 +0200

    Работа с компонентами

--- chapter2-pr1/src/app/app.module.ts
+++ chapter2-pr1/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { ParentComponent } from './working-with-components/parent/parent.component';
+import { ChildComponent } from './working-with-components/child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
